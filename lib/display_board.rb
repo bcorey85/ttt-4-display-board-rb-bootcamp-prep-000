e765030fb76fa698f3bb9d83348d871120f37506
brandon corey <bcorey85@gmail.com>
2019-04-07 20:54:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-blankboard = 
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
-def display_board(board = )
+def display_board(state = board)
+  console
