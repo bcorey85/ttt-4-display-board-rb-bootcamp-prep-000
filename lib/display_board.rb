592a7b8d403b1670997925a51d72f8b605eccb93
brandon corey <bcorey85@gmail.com>
2019-04-07 20:56:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,9 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(state = board)
   puts "#{state[0]} | #{state[1]} | #{state[2]}
-  puts
+  puts "-----------""
   puts "#{state[3]
+  puts "-----------""
   puts
   
 end
